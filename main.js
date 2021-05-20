e1b5f75540ac788eda9b401cd7b1c8421485e536
Franco Neuville <franneuville@gmail.com>
2021-05-20 10:01:03 -0300

    menu responsive

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict'; 
+
+var botonNavbar = document.querySelector("#myTopnav")
+
+botonNavbar.addEventListener("click", desplegarMenu)
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function desplegarMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
