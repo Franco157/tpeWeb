6289705997cbe2861853f00b6c7f81347f1c3b11
Franco Neuville <franneuville@gmail.com>
2021-05-21 14:56:36 -0300

    pantalla giratoria

---
+++ js/envios.js
@@ -0,0 +1,16 @@
+let sucursales = [{
+  "sucursal" : "Buenos Aires",
+  "direccion": "Cabildo 3550",
+  "horario": "8hs a 20hs",
+  "id": "0"
+}, {
+    "sucursal" : "Salta",
+    "direccion": "9 de julio 2342",
+    "horario": "8hs a 13hs - 17hs a 21hs",
+    "id": "1"
+  }, {
+    "sucursal" : "Bahia Blanca",
+    "direccion": "Rivadavia 1151",
+    "horario": "8hs a 13hs - 17hs a 21hs",
+    "id": "2"
+  }]
