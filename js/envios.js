3cd50f60cb531b792358ed2e7aeaed3b65ed8acd
Franco Neuville <franneuville@gmail.com>
2021-06-28 12:08:59 -0300

    tpe3

--- js/envios.js
+++ js/envios.js
@@ -1,86 +1,58 @@
+const url = "https://60cb92e121337e0017e45152.mockapi.io/usuarios/Sucursales";
+
 // BOTONES
 
 let btnAgregar = document.querySelector(".btnAgregar");
-btnAgregar.addEventListener("click", agregar);
+btnAgregar.addEventListener("click", subirDatos);
 
 let btnAgregar3 = document.querySelector(".btnAgregar3");
-btnAgregar3.addEventListener("click", agregar);
+btnAgregar3.addEventListener("click", subirDatos);
 
 let btnEliminar = document.querySelector(".btnEliminar");
-btnEliminar.addEventListener("click", eliminarSucursal);
+btnEliminar.addEventListener("click", borrarSucursal);
+
+// falta crear boton editar
+
+//let btnEditar = document.querySelector(".btnEditar");
+//btnEditar.addEventListener("click", editarSucursal);
 
 // OBJETOS JSON
 
 class modelosSucursal {
-  constructor(id, nombre, direccion, horario) {
-    this.id = id;
+  constructor(nombre, direccion, horario) {
     this.nombre = nombre;
     this.direccion = direccion;
     this.horario = horario;
   }
 }
 
-let sucursalBsAs = new modelosSucursal(
-  "1",
-  "Buenos Aires",
-  "Cabildo 2850",
-  "10 a 20 hs"
-);
-
-let sucursalBahia = new modelosSucursal(
-  "2",
-  "Bahia",
-  "9 de julio  1577",
-  " 8:30 a 13 - 16 a 21 hs"
-);
-
-let sucursalSalta = new modelosSucursal(
-  "3",
-  "Salta",
-  "Moreno 269",
-  "9 a 20 hs"
-);
-
 let sucursales = [];
-
-sucursales.push(sucursalBsAs);
-sucursales.push(sucursalBahia);
-sucursales.push(sucursalSalta);
 
 //CARGAR TABLA
 
-function cargarTabla() {
-  for (let i = 0; i < sucursales.length; i++) {
-    const sucursal = sucursales[i];
-    if (sucursales[i].nombre == "Buenos Aires") {
-      tablaSucursales.innerHTML += `<tr class="filaPintada" >
-      <td><b>${sucursal.id} </b></td>
-      <td>${sucursal.nombre}</td>
-      <td>${sucursal.direccion}</td>
-      <td>${sucursal.horario}</td>
-      </tr>`;
-    } else {
-      tablaSucursales.innerHTML += `<tr>
-  <td><b>${sucursal.id} </b></td>
-  <td>${sucursal.nombre}</td>
-  <td>${sucursal.direccion}</td>
-  <td>${sucursal.horario}</td>
-  </tr>`;
+async function traerDatos() {
+  let tabla = document.querySelector("#tablaSucursales");
+  tabla.innerHTML = "";
+  try {
+    let res = await fetch(url); // GET url
+    let json = await res.json(); // texto json a objeto
+    let sucursales = json;
+    for (const sucursal of sucursales) {
+      tabla.innerHTML += `<tr>
+            <td>${sucursal.id}</td>
+            <td>${sucursal.ciudad}</td>
+            <td>${sucursal.direccion}</td>
+            <td>${sucursal.horario}</td>
+        </tr>`;
     }
+  } catch (event) {
+    console.log(event);
   }
 }
 
-cargarTabla();
-
 // AGREGAR SUCURSAL
 
-function vaciarInputs() {
-  formCiudad.value = "";
-  formDireccion.value = "";
-  formHorario.value = "";
-}
-
-function agregar() {
+async function subirDatos() {
   let quantity;
   console.log(this);
   if (this === btnAgregar) {
@@ -88,32 +60,81 @@ function agregar() {
   } else if (this === btnAgregar3) {
     quantity = 3;
   }
-
   if (
     formCiudad.value != "" &&
     formDireccion.value != "" &&
     formHorario.value != ""
   ) {
+    let sucursal = {
+      ciudad: formCiudad.value,
+      direccion: formDireccion.value,
+      horario: formHorario.value,
+    };
+
     for (let i = 0; i < quantity; i++) {
-      let sucursal_agregada = new modelosSucursal(
-        sucursales.length + 1,
-        formCiudad.value,
-        formDireccion.value,
-        formHorario.value
-      );
-      sucursales.push(sucursal_agregada);
+      try {
+        await fetch(url, {
+          method: "POST",
+          headers: { "Content-type": "application/json" },
+          body: JSON.stringify(sucursal),
+        });
+      } catch (event) {
+        console.log(event);
+      }
     }
+    traerDatos();
     vaciarInputs();
-    tablaSucursales.innerHTML = "";
-    cargarTabla();
   } else {
     alert("Completa los campos");
   }
 }
 
+function vaciarInputs() {
+  formCiudad.value = "";
+  formDireccion.value = "";
+  formHorario.value = "";
+}
+
 // ELIMINAR SUCURSAL
 
-function eliminarSucursal() {
-  sucursales = [];
-  tablaSucursales.innerHTML = "";
+async function borrarSucursal() {
+  try {
+    let res = await fetch(`${url}/${id}`, {
+      method: "DELETE",
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
+
+// MODIFICAR SUCURSAL
+
+async function editarSucursal() {
+  if (
+    formCiudad.value != "" &&
+    formDireccion.value != "" &&
+    formHorario.value != ""
+  ) {
+    let sucursal = {
+      ciudad: formCiudad.value,
+      direccion: formDireccion.value,
+      horario: formHorario.value,
+    };
+    try {
+      await fetch(`${url}/${id}`, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(sucursal),
+      });
+    } catch (event) {
+      console.log(event);
+    }
+
+    traerDatos();
+    vaciarInputs();
+  } else {
+    alert("Completa los campos");
+  }
+}
+
+traerDatos();
